024b23527bee44c69392a3b9ba1d6671b044f386
koyama <koyama.shoichi@gmail.com>
2019-12-26 18:24:27 +0900

    modified main.m

--- main.m
+++ main.m
@@ -4,11 +4,11 @@
 run('./narrowband/source_sensor_placement.m');
 run('./narrowband/reproduction.m');
 
-%Narrowband case for regular placement with internal points (Fig.10-14)
+%Narrowband case for regular placement with internal points (Fig.10-14) - full ATF data is required
 % run('./narrowband/source_sensor_placement_eimint.m');
 % run('./narrowband/reproduction_eimint.m');
 
-%Narrowband case for equiangular placement (Fig.15-16)
+%Narrowband case for equiangular placement (Fig.15-16) - full ATF data is required
 % run('./narrowband/source_sensor_placement_eqang.m');
 % run('./narrowband/reproduction_eqang.m');
 
@@ -16,6 +16,6 @@ run('./narrowband/reproduction.m');
 % run('./broadband/source_sensor_placement.m');
 % run('./broadband/reproduction.m');
 
-%Broadband case w/ position error (Fig.22)
+%Broadband case w/ position error (Fig.22) - full ATF data is required
 % run('./broadband/source_sensor_placement.m');
 % run('./broadband/reproduction_poserr.m');
